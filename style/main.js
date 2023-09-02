d74e00d9a684240f1484cff02100c0867eaa01a7
ashrafulrifaz <developerashrafulislamrifaz@gmail.com>
2023-09-02 13:11:55 +0600

    completed post

--- style/main.js
+++ style/main.js
@@ -7,52 +7,75 @@ let loadData = async () => {
 
    tabItem.forEach(item => {
       let btn = document.createElement('div')
-      btn.innerHTML = `<button onclick="showPosts('${item.category_id}')" class="text-sm py-2 px-5 rounded bg-[#25252533] font-medium text-[#252525]">${item.category}</button>`;
+      btn.innerHTML = `<button onclick="loadPosts('${item.category_id}')" class="text-sm py-2 px-5 rounded bg-[#25252533] font-medium text-[#252525]">${item.category}</button>`;
       tabContainer.appendChild(btn)
-      console.log(item);
    })
-
-   showPosts()   
+   showPosts()
 }
 
-let showPosts = async (id = 1000) => {
-   let res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
+let postContainer = document.getElementById('post-container')
+let emptyContainer = document.getElementById('empty-posts-container')
+
+let allPosts = null;
+const loadPosts = async (category_id=1000)=>{
+   let res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${category_id}`)
    let data = await res.json();
-   let posts = data.data
-   console.log(posts);
+   let posts = data.data;
+   allPosts = posts;
+   showPosts(posts);
+}
+let showPosts = (posts) => {   
+   if(posts.length === 0){
+      emptyContainer.classList.remove('hidden')
+   } else {
+      emptyContainer.classList.add('hidden')
+   }
 
-   let postContainer = document.getElementById('post-container')
    postContainer.innerText = ''
-
-   let verifyImg = document.getElementById('verify_img')
 
    posts.forEach(post => {
       let card = document.createElement('div')
-      console.log(post);
-      
+      // console.log(post);
       card.innerHTML = `
          <div class="relative">
             <img src="${post?.thumbnail}" class="rounded w-full h-44">
-            <span class="text-[10px] text-white py-1 px-1.5 bg-[#171717] rounded absolute bottom-2.5 right-2.5">3hrs 56 min ago</span>
+            <div id="posted-date">
+               <p class="text-[10px] text-white bg-[#171717] rounded absolute bottom-2.5 right-2.5 py-1 px-1.5">${getDate(post.others.posted_date)}</p>
+            </div>
          </div>
          <div class="flex pt-5 gap-2">
             <img src="${post.authors[0]?.profile_picture}" class="w-10 h-10 rounded-full">
             <div>
                <h6 class="text-[#171717] text-[15px] font-semibold leading-normal">Building a Winning UX Strategy Using the Kano Model</h6>
-               <p class="items-center my-1.5 text-sm text-[#171717B2] flex gap-1.5">${post.authors[0]?.profile_name} <span id="verify_img"><img src="img/fi_10629607.png"></span></p>
+               <p class="items-center my-1.5 text-sm text-[#171717B2] flex gap-1.5">${post.authors[0]?.profile_name} <span><img src="${post.authors[0]?.verified ? 'img/fi_10629607.png' : ''}"></span></p>
                <p class="text-sm text-[#171717B2]">${post.others.views}</p>
             </div>
          </div>
       `
-      // if(!!post.authors[0].verified){
-      //    verifyImg.classList.add('inline')
-      //    verifyImg.classList.remove('hidden')
-      // } else {
-      //    verifyImg.classList.remove('inline')
-      //    verifyImg.classList.add('hidden')
-      // }
-      postContainer.appendChild(card)
-   })
+      postContainer.appendChild(card)      
+
+      let timeDiv = card.childNodes[1].childNodes[3];
+      if(post.others.posted_date !== "") {
+         timeDiv.style.display = 'block'
+      } else {
+         timeDiv.style.display = 'none'
+      };
+   })   
 }
 
+loadPosts();
 loadData()
+
+function getDate(date){
+   let hours = Math.floor(date / (60* 60))
+   let minutes = Math.floor((date / 60) % 60)
+   let postedDate = `${hours}hrs ${minutes} min ago`
+   return postedDate
+}
+
+function shortData(){
+   allPosts.sort(
+      (a, b) => b.others.views.split("K")[0] - a.others.views.split("K")[0]
+   );
+   showPosts(allPosts);
+}
